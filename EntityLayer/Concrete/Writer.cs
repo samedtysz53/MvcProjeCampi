7a50e65381084ac24ec24112cb0b206c85a12d08
tmgam <tmgam@DESKTOP-K2PD6PL>
2022-08-27 01:10:43 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Writer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Writer
+    {
+        [Key] //anahtar kelime
+        public int WriteID { get; set; }
+        [StringLength(50)] // max uzunluk
+        public string WriterName { get; set; }
+        [StringLength(50)]
+        public string WriterSurname{ get; set; }
+        [StringLength(100)]
+        public string WriterImage{ get; set; }
+        [StringLength(50)]
+        public string WriterMail{ get; set; }
+        [StringLength(20)]
+        public string WriterPassword{ get; set; }
+        public ICollection<Heading> Headings { get; set; }
+         public ICollection<Content> Contents { get; set; }
+    }
+}
