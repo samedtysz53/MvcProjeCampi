003c68a8cde89b6ec9d17361f9e37e50d153dca9
samed_tysz <samed_tysz@hotmail.com>
2022-08-29 02:41:39 +0300

    new

--- MvcProjeCampi/Controllers/CategoryController.cs
+++ MvcProjeCampi/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,16 @@ namespace MvcProjeCampi.Controllers
 {
     public class CategoryController : Controller
     {
+        CategoryManager cm = new CategoryManager();
         // GET: Category
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult GetCategoryList() 
+        {
+            var CategoryValues = cm.GetAllB();
+            return View(CategoryValues);
         }
     }
 }
