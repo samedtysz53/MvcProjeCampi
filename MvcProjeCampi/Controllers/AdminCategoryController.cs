e138706391ba253f79d36a89c04e3b30f78e21bd
samed_tysz <samed_tysz@hotmail.com>
2022-08-31 23:57:03 +0300

    news versions

--- MvcProjeCampi/Controllers/AdminCategoryController.cs
+++ MvcProjeCampi/Controllers/AdminCategoryController.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFrameWork;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,8 +14,36 @@ namespace MvcProjeCampi.Controllers
     public class AdminCategoryController : Controller
     {
         // GET: AdminCategory
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
         public ActionResult Index()
         {
+            var CategoryValues = cm.GetList();
+            return View(CategoryValues);
+        }
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public ActionResult AddCategory(Category p)
+        {
+            //  cm.CategoryAddB(p);
+            CategoryValidator categoryValidator = new CategoryValidator();
+            ValidationResult result = categoryValidator.Validate(p);
+            if (result.IsValid)
+            {
+                cm.CategoryAdd(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
             return View();
         }
     }
