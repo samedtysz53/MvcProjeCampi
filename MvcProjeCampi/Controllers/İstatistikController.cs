0cc8a663dae1cc15dd155d36f648e376374235e0
samed tüysüz <49450735+samedtysz53@users.noreply.github.com>
2022-09-18 17:34:31 +0300

    new

--- MvcProjeCampi/Controllers/İstatistikController.cs
+++ MvcProjeCampi/Controllers/İstatistikController.cs
@@ -16,14 +16,14 @@ namespace MvcProjeCampi.Controllers
         {
 
             var CategoriCount = context.categories.Count();
-           // var YazılımCount = context.Headings.Count(x => x.Category.Heading.Count);
+            var YazılımCount = context.Headings.Count(x => x.Category.CategoryID == 10);
             var write = context.Writers.Count(x => x.WriterName.Contains("a"));
             var CategoriMaxHeading = context.Headings.Max(x => x.Category.CategoryName);
             var TrueCount = context.categories.Count(x => x.CategoryStatus == true);
             var FalseCount = context.categories.Count(x => x.CategoryStatus == false);
           
             ViewBag.CategoryCount = CategoriCount;
-          //  ViewBag.Heading = YazılımCount;
+            ViewBag.Heading = YazılımCount;
             ViewBag.Writer = write;
             ViewBag.HeadingMax = CategoriMaxHeading;
             ViewBag.StatusFark = (TrueCount - FalseCount);
